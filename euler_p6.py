b78048e09795e06e0abbdf6ea7d6c293320d78f4
mishra14 <mishra14@gmail.com>
2017-01-09 02:57:10 +0000

    problem 6 solution

---
+++ euler_p6.py
@@ -0,0 +1,13 @@
+x = 101
+
+sum_sqr = 0
+sum_of_sqr = 0
+
+for i in xrange(x):
+	sum_sqr = sum_sqr + i
+	sum_of_sqr = sum_of_sqr + i*i
+
+sum_sqr = sum_sqr * sum_sqr
+
+print sum_sqr - sum_of_sqr
+print 'done'
