029da157e16fefb28bbf11ea9368a8944438e162
mishra14 <mishra14@gmail.com>
2017-01-09 04:57:54 +0000

    Added non-tested solutionj to problem 3

---
+++ euler_p3.py
@@ -0,0 +1,13 @@
+import prime_numbers
+import sys
+
+x = 600851475143
+
+primes = prime_numbers.get_primes(int(x/2))
+
+primes.reverse();
+
+for i in primes:
+	if(x%i == 0):
+		print "Answer: {}".format(i)
+		sys.exit()
